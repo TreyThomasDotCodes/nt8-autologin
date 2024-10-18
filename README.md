f53df31968ea3d13076450952ae860bc79866e59
Trey Thomas <trey@treythomas.codes>
2024-10-18 12:56:59 -0500

    First pass

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
+<!--
+SPDX-FileCopyrightText: © 2024 Trey Thomas <trey@treythomas.codes>
+	
+SPDX-License-Identifier: MPL-2.0
+-->
+
 # nt8-autologin
-Automatically launch and log into NinjaTrader 8
+Very quick and dirty app to automatically launch and log into NinjaTrader 8 in non-multi-provider mode.
+More to come. Probably.
+
+## Environment Vars
+
+* NT8A_PATH - path to NinjaTrader.exe
+* NT8A_USER - NT username
+* NT8A_PASS - NT password
+* NT8A_LIVE - TRUE for live trading, FALSE for sim
---
+++ TTDC.Nt8Automater/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * SPDX-FileCopyrightText: © 2024 Trey Thomas <trey@treythomas.codes>
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+using FlaUI.Core;
+using FlaUI.Core.AutomationElements;
+using FlaUI.Core.Tools;
+using FlaUI.UIA3;
+
+string path = Environment.GetEnvironmentVariable("NT8A_PATH") ?? "";
+if (args.Length > 0)
+    path = args[0];
+
+using var automation = new UIA3Automation();
+using var app = Application.AttachOrLaunch(new System.Diagnostics.ProcessStartInfo(path));
+
+var window = app.GetMainWindow(automation);
+var tbUser = window.FindFirstDescendant(cf => cf.ByAutomationId("tbUserName")).AsTextBox();
+var tbPass = window.FindFirstDescendant(cf => cf.ByAutomationId("passwordBox")).AsTextBox();
+var btnLogin = window.FindFirstDescendant(cf => cf.ByAutomationId("btnLogin")).AsButton();
+
+tbUser.Text = Environment.GetEnvironmentVariable("NT8A_USER");
+tbPass.Focus();
+tbPass.Text = Environment.GetEnvironmentVariable("NT8A_PASS");
+btnLogin.Focus();
+btnLogin.Invoke();
+
+var btnLive = Retry.WhileNull(() => window.FindFirstDescendant(cf => cf.ByAutomationId("btnLiveTrading")).AsButton(), TimeSpan.FromSeconds(10));
+var btnSim = Retry.WhileNull(() => window.FindFirstDescendant(cf => cf.ByAutomationId("btnSimulation")).AsButton(), TimeSpan.FromSeconds(10));
+
+if (bool.Parse(Environment.GetEnvironmentVariable("NT8A_LIVE") ?? "")) btnLive.Result.Invoke(); else btnSim.Result.Invoke();
+
